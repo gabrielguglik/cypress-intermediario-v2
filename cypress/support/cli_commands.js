a4f690b7c4f9d2730d07cd096bed4d36509ab4d3
gabrielguglik <gabrielguglielmik@gmail.com>
2024-10-14 11:02:45 -0300

    refactor: trying to fix ssh problems but failling

--- cypress/support/cli_commands.js
+++ cypress/support/cli_commands.js
@@ -1,9 +1,7 @@
 // not including git clone test because of ssh key problems
 
-// Cypress.Commands.add('clone_via_SSH', project_infos => {
-//     const domain = Cypress.config('baseUrl').replace('http://', '')
+Cypress.Commands.add('clone_via_SSH', project_infos => {
+    const domain = Cypress.config('baseUrl').replace('http://', '')
   
-//     cy.exec(`cd cypress/downloads`);
-//     cy.exec(`git clone git@${domain}:${Cypress.env('user_name')}/${project_infos.name}.git`);
-
-  
+    cy.exec(`cd cypress/downloads/ && git clone git@${domain}:${Cypress.env('user_name')}/${project_infos.name}.git`)
+});
